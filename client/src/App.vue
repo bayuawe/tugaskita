ac543d6cd6ed07a33fbc08a62d2d5f4266dd2bde
bayuawe <bayuaryandi21@gmail.com>
2024-11-14 10:31:25 +0700

    setup frontend

---
+++ client/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MainButton from './components/MainButton.vue'
+const name = 'App'
+</script>
+
+<template>
+  <MainButton />
+  <MainButton />
+  <MainButton />
+  <MainButton />
+  <h1>{{ name }}</h1>
+  <h1>{{ name }}</h1>
+  <h1>{{ name }}</h1>
+</template>
+
+<style scoped></style>
